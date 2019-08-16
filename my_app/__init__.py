3192cbf3a77cca2575ef69379d301c80ba916faf
unknown <howwwwwhy@gmail.com>
2019-08-16 23:10:38 +0900

    add 'app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False' to __init__.py

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -8,6 +8,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('CLEARDB_DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
